f24c0ecf9dd8e04dc2fb0877f664387d91ce9b58
Guy Rozendorn <guy@rzn.co.il>
2011-12-28 14:12:43 +0200

    copied core from devicemamanger

---
+++ src/infi/wioctl/structures.py
@@ -0,0 +1,27 @@
+from infi.instruct import ULInt64, ULInt32, ULInt16, ULInt8, Field, FixedSizeArray, FixedSizeString
+from infi.instruct import Struct
+
+def is_64bit():
+    import sys
+    return sys.maxsize > 2 ** 32
+
+POINTER = ULInt64 if is_64bit() else ULInt32
+WORD = ULInt16
+DWORD = ULInt32
+BYTE = ULInt8
+ULONG = DWORD
+UCHAR = BYTE
+BOOLEAN = BYTE
+ULONG64 = ULInt64
+WCHAR = ULInt16
+PARTITION_STYLE = ULInt32
+
+class STORAGE_DEVICE_NUMBER(Struct):
+    _fields_ = [DEVICE_TYPE("DeviceType"), DWORD("DeviceNumber"), DWORD("PartitionNumber")]
+
+class LARGE_INTEGER(Struct):
+    _fields_ = [ULInt64("QuadPart"), ] if is_64bit() else [DWORD("LowPart"), DWORD("HighPart")]
+
+class GUID(Struct):
+    _fields_ = [DWORD("Data1"), WORD("Data2"), WORD("Data3"), FixedSizeArray("Data4", 8, BYTE)]
+
