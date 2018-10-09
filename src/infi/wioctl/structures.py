7b9ff3d3538131f830b6458c78fc2176762fe770
Ory Jonay <ojonay@infinidat.com>
2018-10-09 13:12:41 +0300

    HPT-2283 code review HPT-23 changes
    Added __eq__ method to GUID class

--- src/infi/wioctl/structures.py
+++ src/infi/wioctl/structures.py
@@ -21,3 +21,7 @@ class LARGE_INTEGER(Struct):
 class GUID(Struct):
     _fields_ = [DWORD("Data1"), WORD("Data2"), WORD("Data3"), FixedSizeArray("Data4", 8, BYTE)]
 
+    def __eq__(self, other):
+        if not isinstance(other, GUID):
+            return False
+        return all(getattr(self, 'Data%d' % i) == getattr(other, 'Data%d' % i) for i in range(1, 5))
